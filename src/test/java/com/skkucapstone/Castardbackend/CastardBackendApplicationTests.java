7e19685555b84467758b1e3996b56109222b96ee
nicedavid98 <nicedavid98@daum.net>
2024-04-26 14:25:46 +0900

    SpringBoot 프로젝트 생성.

---
+++ src/test/java/com/skkucapstone/Castardbackend/CastardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skkucapstone.Castardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
