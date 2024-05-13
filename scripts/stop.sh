18e9edddf721936b24e31cb84c70999846034e6a
ByungChan Bae <124234976+nicedavid98@users.noreply.github.com>
2024-05-13 23:26:08 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ubuntu/spring-github-action"
+ROOT_PATH="/home/ubuntu/Castard-backend"
 JAR="$ROOT_PATH/application.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
 if [ -z "$SERVICE_PID" ]; then
-  echo "서비스 NouFound" >> $STOP_LOG
+  echo "서비스 Not Found" >> $STOP_LOG
 else
   echo "서비스 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
