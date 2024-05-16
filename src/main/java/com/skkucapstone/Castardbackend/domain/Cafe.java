0f924f80ec64f51014026f613008a9a6cffa04f1
nicedavid98 <nicedavid98@daum.net>
2024-05-17 00:04:55 +0900

    카페 리뷰 평점 유지 로직 변경 (합을 유지-> 평균을 유지)

--- src/main/java/com/skkucapstone/Castardbackend/domain/Cafe.java
+++ src/main/java/com/skkucapstone/Castardbackend/domain/Cafe.java
@@ -25,15 +25,15 @@ public class Cafe {
 
     private Long latitude; // y좌표
 
-    // 카공에 대한 리뷰들의 총합을 유지하는 필드들.
-    private Long power_socket = 0L;
-    private Long capacity = 0L;
-    private Long quiet = 0L;
-    private Long wifi = 0L;
-    private Long tables = 0L;
-    private Long toilet = 0L;
-    private Long bright = 0L;
-    private Long clean = 0L;
+    // 카공에 대한 리뷰들의 평균을 유지하는 필드들.
+    private Double power_socket = 0D;
+    private Double capacity = 0D;
+    private Double quiet = 0D;
+    private Double wifi = 0D;
+    private Double tables = 0D;
+    private Double toilet = 0D;
+    private Double bright = 0D;
+    private Double clean = 0D;
 
     // 카공에 대한 리뷰들의 개수를 유지하는 필드들. (평균 계산에 활용됨)
     private Long power_socket_cnt = 0L;
@@ -50,36 +50,36 @@ public class Cafe {
 
     public Cafe addCafeRatings(Review review) {
         if (review.getPower_socket() != 0) {
-            power_socket += review.getPower_socket();
             power_socket_cnt += 1;
+            power_socket = (power_socket + review.getPower_socket()) / power_socket_cnt;
         }
         if (review.getCapacity() != 0) {
-            capacity += review.getCapacity();
             capacity_cnt += 1;
+            capacity = (capacity + review.getPower_socket()) / capacity_cnt;
         }
         if (review.getQuiet() != 0) {
-            quiet += review.getQuiet();
             quiet_cnt += 1;
+            quiet = (quiet + review.getPower_socket()) / quiet_cnt;
         }
         if (review.getWifi() != 0) {
-            wifi += review.getWifi();
             wifi_cnt += 1;
+            wifi = (wifi + review.getPower_socket()) / wifi_cnt;
         }
         if (review.getTables() != 0) {
-            tables += review.getTables();
             tables_cnt += 1;
+            tables = (tables + review.getPower_socket()) / tables_cnt;
         }
         if (review.getToilet() != 0) {
-            toilet += review.getToilet();
             toilet_cnt += 1;
+            toilet = (toilet + review.getPower_socket()) / toilet_cnt;
         }
         if (review.getBright() != 0) {
-            bright += review.getBright();
             bright_cnt += 1;
+            bright = (bright + review.getPower_socket()) / bright_cnt;
         }
         if (review.getClean() != 0) {
-            clean += review.getClean();
             clean_cnt += 1;
+            clean = (clean + review.getPower_socket()) / clean_cnt;
         }
 
         return this;
